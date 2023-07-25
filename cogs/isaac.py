61b064e4caaf764cae218a7f272406d401b08c5a
Lars Andreas Lillehaug <Lars12391@gmail.com>
2023-07-25 16:31:37 +0200

    Fix isaac arrow rotation, still need to move it out from center

--- cogs/isaac.py
+++ cogs/isaac.py
@@ -52,40 +52,49 @@ def edit_image(image, color):
     yresize = random.randint(int(shortest_length/10), int(shortest_length/7))
     # Isaac
     isaac = Image.open("images/isaac.png").resize((int(xresize*0.8), int(yresize*0.8)))
-    w, h = isaac.size
+    iw, ih = isaac.size
     
     # Get circle alpha, apply alpha to new colored image
     # Bad for performance but it works. Rework into tinting instead
     white_circle = Image.open("images/whiteCircle.png").resize((xresize, yresize))
     alpha = white_circle.getchannel('A')
-    colored_circle = Image.new('RGBA', white_circle.size, color=color)
+    colored_circle = Image.new('RGBA', white_circle.size, color="red")
     colored_circle.putalpha(alpha) 
+    cw, ch = colored_circle.size
+    # Paste Isaac into middle of circle
+    colored_circle.paste(isaac,(int((cw-iw)/2), int((ch-ih)/2)), mask=isaac)
+    
+    # Offset for placing Isaac
+    xoff = random.randint(0, image.size[0]-cw)
+    yoff = random.randint(0, image.size[1]-ch)
+    
+    # Place Isaac
+    image.paste(colored_circle, (xoff, yoff), mask=colored_circle)
+    
+    c_center = (int(xoff + cw/2), int(yoff + ch/2))
+    print(c_center)
     
     
     # Arrow
-    white_arrow = Image.open("images/whiteArrow.png").resize((xresize*2, yresize))
-    # Rotate
-    arrow_vector = create_vector(max(colored_circle.size))
-    white_arrow = rotate_image(white_arrow, arrow_vector)
-    
+    white_arrow = Image.open("images/whiteArrow.png").resize((int(xresize*6), int(yresize/2)))
     alpha = white_arrow.getchannel('A')
-    colored_arrow = Image.new('RGBA', white_arrow.size, color="red")
+    colored_arrow = Image.new('RGBA', white_arrow.size, color=color)
     colored_arrow.putalpha(alpha) 
-    
-    cw, ch = colored_circle.size
-    
-    colored_circle.paste(isaac,(int((cw-w)/2), int((ch-h)/2)), mask=isaac)
 
-    xoff = random.randint(0, image.size[0]-cw)
-    yoff = random.randint(0, image.size[1]-ch)
-
+    arrow_vector = create_vector(max(colored_circle.size))
+    angle_rad = math.atan2(arrow_vector[1], arrow_vector[0])
+    angle_deg = math.degrees(angle_rad)
     
-    image.paste(colored_circle, (xoff, yoff), mask=colored_circle)
+    rotated_arrow = colored_arrow.rotate(angle_deg, expand=True)
     
-
     
-    image.paste(colored_arrow, (int(xoff + cw/2 + arrow_vector[0]), int(yoff + arrow_vector[1])), mask=colored_arrow)
+    
+    print("Arrow vector")
+    print(arrow_vector)
+    
+    image.paste(rotated_arrow, (int(c_center[0] - rotated_arrow.width/2), int(c_center[1] - rotated_arrow.height/2)), mask=rotated_arrow)
     image.save("images/temp/image2.png")
+    
     
     
 async def get_avatar(interaction):
@@ -118,16 +127,7 @@ def create_vector(length):
 
     return (scaled_x, scaled_y)
     
-    
-def rotate_image(image, vector):
-    angle_rad = math.atan2(vector[1], vector[0])
-    angle_deg = math.degrees(angle_rad)
 
-    rotated_image = image.rotate(angle_deg, resample=Image.BICUBIC, expand=True)
-
-    return rotated_image
-    
-    
     
     
     
